9a64c290d283ce5d5afb9fe5f60458f31ba08248
iamjuant <42824704+iamjuant@users.noreply.github.com>
2021-04-02 00:25:29 -0600

    added defaultprops

--- counter-app/src/FirstApp.js
+++ counter-app/src/FirstApp.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 import './index.css'
 import PropTypes from 'prop-types'
 
-const FirstApp = ({saludo = 'Hello World'})=>{
+const FirstApp = ({saludo = 'Hello World', subtitulo})=>{
     //when you want to return several things you can put them in div tags
     //also you can import fragment tag so you dont have to have a lot of divs
     // or you can use <> </> empty, this is the same as fragment
@@ -12,12 +12,18 @@ const FirstApp = ({saludo = 'Hello World'})=>{
     return <>
             <h1>{saludo}</h1>    
             {/* <pre> {JSON.stringify(saludo,null,3)}</pre> */}
-            <p>My First Application</p>
+            <p>{subtitulo}</p>
         </>
 };
 
 //using proptypes is to force a developer to send a prop in a specific way ex. String, required, number.
 FirstApp.propTypes = {
     saludo: PropTypes.string
+}
+
+//We can use defaultProps to assign default properties and they can be viewed in the props section 
+//even if not passed
+FirstApp.defaultProps ={
+    subtitulo: 'soy un subtitulo'
 }
 export default FirstApp;
