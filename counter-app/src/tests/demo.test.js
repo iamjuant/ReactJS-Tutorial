4c6650da5971ecbe90673bb466d2b6adce53d5d0
iamjuant <42824704+iamjuant@users.noreply.github.com>
2021-04-11 00:34:36 -0600

    added first test with describe

--- counter-app/src/tests/demo.test.js
+++ counter-app/src/tests/demo.test.js
@@ -1,4 +1,14 @@
+describe('Test on the file demo.test.js',()=>{
 
-test('this is my first test',()=>{
-    const isActive = true;
-})
+    test('this is my first test',()=>{
+        const message= 'Hello World';
+        const message2 = `Hello World`;
+    
+        expect(message).toBe(message2);
+    
+    });
+
+
+
+
+});
